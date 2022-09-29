6748df04988a07f7443c99bdc80d8e2e501c56fc
Srikanth1314 <srikanthmaturi9737@gmail.com>
2022-09-29 13:13:00 +0530

    prime Problem

---
+++ problem1.js
@@ -0,0 +1,15 @@
+
+
+let n=7
+let count=0
+
+for(let i=0; i<=n; i--){
+    if(num%i=0){
+        count+-
+    }
+}
+if(count==2){
+    console.log("is Prime");
+}else {
+    console.log("is not Prime");
+}
